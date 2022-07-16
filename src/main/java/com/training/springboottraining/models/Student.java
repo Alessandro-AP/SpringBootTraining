b466196062a1212fa2277289574c3b5434666c79
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-07-17 00:01:22 +0200

    Adding models and lombok

--- src/main/java/com/training/springboottraining/models/Student.java
+++ src/main/java/com/training/springboottraining/models/Student.java
@@ -1,2 +1,16 @@
-package com.training.springboottraining.models;public class Student {
+package com.training.springboottraining.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Student {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final Gender gender;
+
+    enum Gender { MALE, FEMALE}
+
 }
